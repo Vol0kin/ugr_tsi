7607bbadfa529dffdb4f2e6da40e7dd86ed1efdc
Vladislav Nikolov <vladis890@gmail.com>
2019-05-17 21:32:00 +0200

    Updated parser for Ej1-Ej2.

--- P2/Generador/generaproblema.py
+++ P2/Generador/generaproblema.py
@@ -15,6 +15,10 @@ npc_obj_dict['Algoritmo'] = set()
 npc_obj_dict['Oro'] = set()
 npc_obj_dict['Rosa'] = set()
 
+# Claves de los jugadores
+player_keys = ['Player']
+npc_keys = ['Leonardo', 'Bruja', 'Principe', 'Princesa', 'Profesor']
+
 # Declarar conjuntos que contendran las zonas
 zones = []
 connections = []
@@ -37,6 +41,9 @@ out_teachers = ''
 out_players = ''
 
 tab_increment = 4
+
+# Variables booleanas
+use_costs = False
 
 with open(sys.argv[1]) as in_file:
 
@@ -72,8 +79,12 @@ with open(sys.argv[1]) as in_file:
             line_zones = re.findall(r'z[1-9]+', zone_list)
             line_objects = re.findall(r'\[.*?\]', zone_list)
 
+            # Obtener las distancias de cada zona
+            costs = re.findall(r'=[0-9]*=', zone_list)
+
             # Eliminar parentesis de los objetos
             formated_objects = [re.sub(r'[\[\]]', '', objects) for objects in line_objects]
+            formated_costs = [re.sub(r'=', '', cost) for cost in costs]
 
             # Obtener numero de zonas
             num_zones = len(line_zones)
@@ -96,14 +107,75 @@ with open(sys.argv[1]) as in_file:
                     for npc_object in objects_list:
                         var, var_class = npc_object.split('-')
                         npc_obj_dict[var_class].add(var)
-                        positions.add('(at {} {})\n'.format(zone, var))
+                        positions.add('(at {} {})\n'.format(var, zone))
+            
+            # Insertar costes (si existen)
+            if len(costs) > 0:
+                use_cost = True
+
+                for i in range(len(costs)):
+                    connections.append('(= (distance {} {}) {})\n'.format(line_zones[i], line_zones[i + 1], formated_costs[i]))
+                    connections.append('(= (distance {} {}) {})\n'.format(line_zones[i + 1], line_zones[i], formated_costs[i]))
+
+
         # Siguiente linea
         line = in_file.readline()
 
 with open(sys.argv[2], 'w') as out_file:
     tab_size = tab_increment
+
+    # Escribir nombre de problema
     out_file.write(problem)
+
+    # Escribir nombre del dominio
     out_file.write('\t'.expandtabs(tab_size) + domain)
+
+    # Escribir variables e incrementar dist. tabulado
+    out_file.write('\t'.expandtabs(tab_size) + '(:objects\n')
+    tab_size += tab_increment
+    
+    out_file.write('\t'.expandtabs(tab_size) + out_zones)
+
+    for class_var, var in npc_obj_dict.items():
+        if not len(var) == 0:
+            out_file.write('\t'.expandtabs(tab_size) + ' '.join(var) + ' - ' + class_var + '\n')
+
+    # Restaurar desplazamiento del tabulado y cerrar variables
+    tab_size -= tab_increment
+    out_file.write('\t'.expandtabs(tab_size) + ')\n')
+
+    # Escribir hechos iniciales e incrementar dist. tabulado
+    out_file.write('\t'.expandtabs(tab_size) + '(:init\n')
+    tab_size += tab_increment
+    
+    # Escribir zonas
+    for connection in connections:
+        out_file.write('\t'.expandtabs(tab_size) + connection)
+    
+    # Escribir objetos y personajes
+    for npc_obj in positions:
+        out_file.write('\t'.expandtabs(tab_size) + npc_obj)
+
+    # Escribir mano vacia para cada jugador y su orientacion
+    for key in player_keys:
+        players = npc_obj_dict[key]
+
+        if len(players) > 0:
+            for player in players:
+                out_file.write('\t'.expandtabs(tab_size) + '(emptyhand {})\n'.format(player))
+                out_file.write('\t'.expandtabs(tab_size) + '(oriented {} S)\n'.format(player))
+
+    # Escribir objetos recibidos de cada personaje
+    for key in npc_keys:
+        npcs = npc_obj_dict[key]
+
+        if len(npcs) > 0:
+            for npc in npcs:
+                out_file.write('\t'.expandtabs(tab_size) + '(= (received {}) 0)\n'.format(npc))
+
+    # Restaurar desplazamiento del tabulado y cerrar hechos iniciales
+    tab_size -= tab_increment
+    out_file.write('\t'.expandtabs(tab_size) + ')\n')
 
     out_file.write(')')
 
